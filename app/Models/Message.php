4b8bd2b58410b3f69f0de893fc71d15beb22126c
Luka Stosic <luka@yandex.com>
2024-05-17 08:37:29 +0200

    generate models fillable and relations

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Mail\Attachment;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "message",
+        "sender_id",
+        "receiver_id",
+        "group_id",
+        "conversation_id"
+    ];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, "sender_id");
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, "receiver_id");
+    }
+
+    public function group()
+    {
+        return $this->belongsTo(Group::class);
+    }
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function attachtments()
+    {
+        return $this->hasMany(MessageAttachment::class);
+    }
 }
